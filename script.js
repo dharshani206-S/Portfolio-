5b315c135643dd6c6d2f770f46772033c9c4c769
Dharshani <dharshadharshu206@gmail.com>
2026-01-15 10:25:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const elements = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
